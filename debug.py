5186b031b4267c6b1046b27a6323c27a00faf508
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2022-01-29 02:50:55 -0500

    Test identifiers

--- debug.py
+++
@@ -1,7 +0,0 @@
-from moasm.pretokenizer.pretokenizer import PreTokenizer
-from moasm.grouper.grouper import Grouper
-
-
-pretokens = PreTokenizer("hello.moasm").tokenize()
-
-groups = Grouper(pretokens).group()
